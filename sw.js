3b18dd9480d6de39ad26ea048c6a4e4482f40f0d
Luis J. Salvatierra <ljsalvatierra@ljsalvatierra.com>
2021-07-06 15:37:59 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-89be744bf6db75fd8e73.js"
+    "url": "webpack-runtime-339ef9dddd8f57e65da3.js"
   },
   {
     "url": "framework-de237a875aa45b276a9d.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ae27cf5aa1ad72b02c72341b490576f8"
+    "revision": "9afc92a4cd588279ceab12ff946caa79"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "ca32688cca8ab59cc97f420facc5f6fc"
+    "revision": "a8dd9f0a5aa4198437b4ec3949b8b933"
   },
   {
     "url": "polyfill-01365ee183870cafc13e.js"
